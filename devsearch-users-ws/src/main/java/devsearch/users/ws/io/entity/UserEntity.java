af29155c3092ec6fb9aa52eb7afa3c7606a16fa3
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 13:58:48 +0200

    Adding getters and setters

--- devsearch-users-ws/src/main/java/devsearch/users/ws/io/entity/UserEntity.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/io/entity/UserEntity.java
@@ -35,4 +35,56 @@ public class UserEntity implements Serializable{
 
     @Column(nullable = false)
     private String encryptedPassword;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 }
