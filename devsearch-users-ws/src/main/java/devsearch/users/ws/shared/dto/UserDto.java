a5f5c4980212886000d8ae5ee2d85223f977ec5d
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 14:01:10 +0200

    Adding simple UserDto

--- devsearch-users-ws/src/main/java/devsearch/users/ws/shared/dto/UserDto.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/shared/dto/UserDto.java
@@ -1,83 +1,57 @@
-package devsearch.users.ws.io.entity;
+package devsearch.users.ws.shared.dto;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "users")
-public class UserEntity implements Serializable{
-
-    private static final long serialVersionUID = 6023605726818532743L;
-
-    @Id
-    @GeneratedValue
+public class UserDto {
+    
     private long id;
-
-    @Column(nullable = false, unique = true)
     private String publicId;
-
-    @Column(nullable = false, length = 50)
     private String firstName;
-
-    @Column(nullable = false, length = 50)
     private String lastName;
-
-    @Column(nullable = false, length = 120, unique = true)
     private String email;
-
-    @Column(nullable = false)
+    private String password;
     private String encryptedPassword;
-
+    
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getPublicId() {
         return publicId;
     }
-
     public void setPublicId(String publicId) {
         this.publicId = publicId;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public String getEncryptedPassword() {
         return encryptedPassword;
     }
-
     public void setEncryptedPassword(String encryptedPassword) {
         this.encryptedPassword = encryptedPassword;
     }
+    
+    
 }
