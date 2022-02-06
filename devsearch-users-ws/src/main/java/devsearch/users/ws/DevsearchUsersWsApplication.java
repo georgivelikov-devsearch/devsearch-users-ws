44bc92de89d9049ab5b5d545f391004fab5d5561
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 13:29:23 +0200

    Adding configuration for MySQL server

--- devsearch-users-ws/src/main/java/devsearch/users/ws/DevsearchUsersWsApplication.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/DevsearchUsersWsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevsearchUsersWsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevsearchUsersWsApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(DevsearchUsersWsApplication.class, args);
+    }
 
 }
