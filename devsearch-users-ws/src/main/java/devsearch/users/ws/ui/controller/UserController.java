bf5edb0afa3e9bcc07f08a0e0d876395cb645bb1
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 14:56:14 +0200

    Adding Utils and createUser in UserController

--- devsearch-users-ws/src/main/java/devsearch/users/ws/ui/controller/UserController.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/ui/controller/UserController.java
@@ -4,12 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import devsearch.users.ws.exception.UsersRestApiException;
 import devsearch.users.ws.service.UserService;
 import devsearch.users.ws.shared.dto.UserDto;
+import devsearch.users.ws.ui.model.request.UserDetailsRequestModel;
 import devsearch.users.ws.ui.model.response.UserResponse;
 
 @RestController
@@ -31,5 +34,13 @@ public class UserController {
 	UserDto userDto = userService.getUserByPublicId(userId);
 
 	return modelMapper.map(userDto, UserResponse.class);
+    }
+
+    @PostMapping
+    public UserResponse createUser(@RequestBody UserDetailsRequestModel userDetails) throws UsersRestApiException {
+	UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+	UserDto createdUser = userService.createUser(userDto);
+
+	return modelMapper.map(createdUser, UserResponse.class);
     }
 }
