1f3a5d93fd4fcb0a6c9bd7ac0517f01edc532383
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 14:12:46 +0200

    Adding some methods in UserService

--- devsearch-users-ws/src/main/java/devsearch/users/ws/service/UserService.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/service/UserService.java
@@ -1,5 +1,25 @@
 package devsearch.users.ws.service;
 
+import java.util.List;
+
+import devsearch.users.ws.exception.UsersRestApiException;
+import devsearch.users.ws.shared.dto.UserDto;
+
 public interface UserService {
 
+    public UserDto getUserByPublicId(String publicId) throws UsersRestApiException;
+
+    public UserDto getUserByUsername(String username) throws UsersRestApiException;
+
+    public UserDto getUserByEmail(String email) throws UsersRestApiException;
+
+    // public UserDto getUserForLogin(String email) throws UsersRestApiException;
+
+    public UserDto updateUser(String publicId, UserDto user) throws UsersRestApiException;
+
+    public UserDto createUser(UserDto user) throws UsersRestApiException;
+
+    public void deleteUser(String publicId) throws UsersRestApiException;
+
+    public List<UserDto> getUsers(int page, int limit) throws UsersRestApiException;
 }
