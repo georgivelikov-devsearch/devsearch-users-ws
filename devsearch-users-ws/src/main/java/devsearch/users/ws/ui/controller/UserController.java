864d2765e9a6834b1354fc2e566039e316fefacc
Georgi Velikov <gmvelikov@gmail.com>
2022-02-06 14:49:48 +0200

    Adding getUser to UserController

--- devsearch-users-ws/src/main/java/devsearch/users/ws/ui/controller/UserController.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/ui/controller/UserController.java
@@ -1,15 +1,35 @@
 package devsearch.users.ws.ui.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import devsearch.users.ws.exception.UsersRestApiException;
+import devsearch.users.ws.service.UserService;
+import devsearch.users.ws.shared.dto.UserDto;
+import devsearch.users.ws.ui.model.response.UserResponse;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
     @GetMapping(path = "/status")
     public String welcome() {
 	return "UserController is working!";
+    }
+
+    @GetMapping(path = "/{publicId}")
+    public UserResponse getUser(@PathVariable String userId) throws UsersRestApiException {
+	UserDto userDto = userService.getUserByPublicId(userId);
+
+	return modelMapper.map(userDto, UserResponse.class);
     }
 }
