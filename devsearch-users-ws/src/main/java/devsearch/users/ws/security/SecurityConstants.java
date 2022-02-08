ee2c927a42f1344b665c2c2c56f7edea0d9dffdd
Georgi Velikov <gmvelikov@gmail.com>
2022-02-08 21:00:40 +0200

    Adding SecurityConstants

---
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/security/SecurityConstants.java
@@ -0,0 +1,21 @@
+package devsearch.users.ws.security;
+
+public class SecurityConstants {
+
+    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final String TOKER_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users";
+    public static final String STATUS_URL = "/status";
+    private static final String tokenSecret = "jf9i4jgu83nfl0"; // Must be unique
+    // private static String tokenSecret = null;
+
+    public static String getTokenSecret() {
+//	if (tokenSecret == null) {
+//	    AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+//	    tokenSecret = appProperties.getTokenSecret();
+//	}
+
+	return tokenSecret;
+    }
+}
