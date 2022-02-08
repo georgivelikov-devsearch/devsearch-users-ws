f8591d70b874d960eb4a87c5f9c26bce19ca0421
Georgi Velikov <gmvelikov@gmail.com>
2022-02-08 20:53:23 +0200

    Adding exception message codes

--- devsearch-users-ws/src/main/java/devsearch/users/ws/exception/UsersRestApiException.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/exception/UsersRestApiException.java
@@ -5,6 +5,7 @@ public class UsersRestApiException extends Exception {
     private static final long serialVersionUID = -2200242070640018947L;
 
     private String sourceExceptionMessage;
+    private String exceptionCode;
 
     public UsersRestApiException(String message) {
 	super(message);
@@ -12,20 +13,31 @@ public class UsersRestApiException extends Exception {
 
     public UsersRestApiException(ExceptionMessages message) {
 	super(message.getExceptionMessage());
+	this.exceptionCode = message.getExceptionCode();
     }
 
     public UsersRestApiException(ExceptionMessages message, String sourceExceptionMessage) {
 	super(message.getExceptionMessage());
+	this.exceptionCode = message.getExceptionCode();
 	this.sourceExceptionMessage = sourceExceptionMessage;
     }
 
-    public UsersRestApiException(String message, String sourceExceptionMessage) {
+    public UsersRestApiException(String message, String exceptionCode, String sourceExceptionMessage) {
 	super(message);
+	this.exceptionCode = exceptionCode;
 	this.sourceExceptionMessage = sourceExceptionMessage;
     }
 
     public String getSourceExceptionMessage() {
 	return this.sourceExceptionMessage;
+    }
+
+    public String getExceptionCode() {
+	return exceptionCode;
+    }
+
+    public void setExceptionCode(String exceptionCode) {
+	this.exceptionCode = exceptionCode;
     }
 
 }
