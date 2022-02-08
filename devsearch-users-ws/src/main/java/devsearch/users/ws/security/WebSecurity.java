63c097a65aa1dde130da3ca499bb8004153295c7
Georgi Velikov <gmvelikov@gmail.com>
2022-02-08 22:41:31 +0200

    Testing commit

--- devsearch-users-ws/src/main/java/devsearch/users/ws/security/WebSecurity.java
+++ devsearch-users-ws/src/main/java/devsearch/users/ws/security/WebSecurity.java
@@ -26,6 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+	// Both authorization and authentication filters are needed for basic JWT
 	http.csrf()
 		.disable()
 		.authorizeRequests()
